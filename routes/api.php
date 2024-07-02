f7803b29014ff3ff72b951bfca338314c2438530
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-02 16:26:52 +0200

    feat: created API Route for show() method of ProjectController - api.php

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/projects', [ProjectController::class, 'index']);
 
+Route::get('/projects/{projectSlug}', [ProjectController::class, 'show']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
